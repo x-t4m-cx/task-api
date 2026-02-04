461ef9d6074a44c1cbc20019f107d2ab8f37fd3f
x-t4m-cx <t4bakovm@yandex.ru>
2026-02-04 20:57:49 +0400

    init

---
+++ src/test/java/com/example/task_api/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
